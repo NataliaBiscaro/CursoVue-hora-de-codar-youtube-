3e5ba137c56c2a391a31988a9f08bb77dfaa1b94
Natalia <nataliabiscaro.18@gmail.com>
2021-12-02 19:33:53 -0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+ <div>
+    <PrimeiroComponente />
+    <SegundoComponente/>
+    <p> {{ dado }} </p>
+ </div>
+</template>
+
+<script>
+  import PrimeiroComponente from './components/PrimeiroComponente.vue'
+  import SegundoComponente from './components/SegundoComponente.vue'
+
+    export default {
+      name: 'App', 
+      components: {
+        PrimeiroComponente,
+        SegundoComponente,
+      },
+      data() {
+        return {
+          dado: "testanduuuu"
+        }
+      } 
+    }
+</script>
---
+++ src/components/SegundoComponente.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <h1> Esse eu fiz sozinha :) </h1>
+    <p> Eu me sinto {{ status }} . </p>
+</div>
+
+</template>
+
+<script>
+    export default{
+        name: 'SegundoComponente',
+        data() {
+            return{
+              status: 'feliz por estar evoluindo'
+            }
+        }
+    }
+</script>
