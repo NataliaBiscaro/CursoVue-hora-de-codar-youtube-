e5eb7d75bd3481c0ab1e387419381856b846977b
Natalia <nataliabiscaro.18@gmail.com>
2021-12-04 00:02:06 -0300

    Hierarquia de componentes aula #8

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <PrimeiroComponente/>
     <SegundoComponente/>
     <Life-cycle/>
+    <Pessoa/>
     <p> {{ dado }} </p>
  </div>
 </template>
@@ -11,14 +12,16 @@
 
   import PrimeiroComponente from './components/PrimeiroComponente.vue';
   import SegundoComponente from './components/SegundoComponente.vue';
-  import LifeCycle from './components/LifeCycle.vue'
+  import LifeCycle from './components/LifeCycle.vue';
+  import Pessoa from './components/Pessoa.vue'
 
     export default {
       name: 'App', 
       components: {
         PrimeiroComponente,
         SegundoComponente,
-        LifeCycle
+        LifeCycle,
+        Pessoa
       },
             LifeCycledata() {
         return {
